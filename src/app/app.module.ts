0195f7ad044ccf14ba4079fb166509a5b6d1ad9c
Adam Dunsmoor <adamdunsmoor@pm.me>
2019-10-19 13:04:52 -0500

    updates for mobile carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // General
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import 'hammerjs';
 
 // Forms
 import { FormsModule } from '@angular/forms';
@@ -27,6 +26,29 @@ import { BibleComponent } from './bible/bible.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ServicesComponent } from './services/services.component';
+
+// HammerJS (MDBootstrap)
+// Source: https://mdbootstrap.com/docs/angular/advanced/mobile/
+declare var Hammer: any;
+export class MyHammerConfig extends HammerGestureConfig {
+    overrides = <any>{
+        'pan': { direction: Hammer.DIRECTION_All },
+        'swipe': { direction: Hammer.DIRECTION_VERTICAL },
+    };
+
+    buildHammer(element: HTMLElement) {
+        const mc = new Hammer(element, {
+            touchAction: 'auto',
+            inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput,
+            recognizers: [
+                [Hammer.Swipe, {
+                    direction: Hammer.DIRECTION_HORIZONTAL
+                }]
+            ]
+        });
+        return mc;
+    }
+}
 
 @NgModule({
     declarations: [
@@ -52,6 +74,10 @@ import { ServicesComponent } from './services/services.component';
     ],
     providers: [
         MDBSpinningPreloader,
+        {
+            provide: HAMMER_GESTURE_CONFIG,
+            useClass: MyHammerConfig
+        }
     ],
     bootstrap: [AppComponent]
 })
