03df0496e2959e4bbd7fd3521460ac416bef4bef
Adam Dunsmoor <adamdunsmoor@pm.me>
2020-04-09 20:15:57 -0500

    pdate packages; angular 8 to 9, add extra pic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 1.0.1 (4/9/2020)
+
+- update packages
+- **Major:** update to Angular 9 from 8
+- add `church_interior_4.JPEG` to `home` carousel
+
 ## 1.0.0 (10/11/2019)
 
 - Website created from xinthose's personal website
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injectable } from '@angular/core';
 
 // General
 import { AppComponent } from './app.component';
@@ -30,6 +30,7 @@ import { ServicesComponent } from './services/services.component';
 // HammerJS (MDBootstrap)
 // Source: https://mdbootstrap.com/docs/angular/advanced/mobile/
 declare var Hammer: any;
+@Injectable()
 export class MyHammerConfig extends HammerGestureConfig {
     overrides = <any>{
         'pan': { direction: Hammer.DIRECTION_All },
